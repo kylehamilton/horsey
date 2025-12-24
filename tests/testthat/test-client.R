0e1f188b72511d70163c950d881bb69bd55bce09
W. Kyle Hamilton <kyle.hamilton@gmail.com>
2025-12-24 14:38:05 -0800

    Add tests for client() quiet parameter and warnings
    Expanded test coverage for the client() function to verify correct behavior of the 'quiet' parameter and warning messages when no token is set.

--- tests/testthat/test-client.R
+++ tests/testthat/test-client.R
@@ -7,6 +7,25 @@ test_that("client() initializes", {
   expect_s3_class(cl, "horsey_client")
   expect_null(cl$token)
   expect_equal(cl$base_url, "https://lichess.org")
+  expect_true(cl$quiet)
+})
+
+test_that("client() correct behavior for quiet", {
+  old <- Sys.getenv("LICHESS_TOKEN")
+  Sys.setenv(LICHESS_TOKEN = "")
+  on.exit(Sys.setenv(LICHESS_TOKEN = old), add = TRUE)
+
+  expect_warning(cl <- client(quiet = FALSE))
+  expect_false(cl$quiet)
+})
+
+test_that("client() correct default for quiet", {
+  old <- Sys.getenv("LICHESS_TOKEN")
+  Sys.setenv(LICHESS_TOKEN = "")
+  on.exit(Sys.setenv(LICHESS_TOKEN = old), add = TRUE)
+
+  expect_warning(cl <- client())
+  expect_false(cl$quiet)
 })
 
 test_that("client() uses the user provided token and base url", {
@@ -15,6 +34,10 @@ test_that("client() uses the user provided token and base url", {
   expect_s3_class(cl, "horsey_client")
   expect_equal(cl$token, "abc123")
   expect_equal(cl$base_url, "https://example.com")
+})
+
+test_that("client() warns about no token found and tells user that some endpoints won't work", {
+  expect_warning(cl <- client())
 })
 
 test_that("client() warns when no token is available", {
